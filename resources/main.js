fa9b175273e3c08dc93dfc7539d8b0859bf77855
Kevin Moore <kevinkmooretech@gmail.com>
2017-02-13 00:51:00 -0800

    Add files via upload

---
+++ resources/main.js
@@ -0,0 +1,10 @@
+
+
+// User clicked on add button
+// if there is text in item field, add that text to the to do list
+document.getElementById('add').addEventListener('click', function() {
+  var value = document.getElementById('item').value;
+  if (value) {
+    
+  }
+ });
